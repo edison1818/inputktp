daa3099147ba84213141d627c1dd7c4cb72a58f6
Edison <36102753+edison1818@users.noreply.github.com>
2020-12-30 11:07:05 +0700

    Add files via upload

---
+++ application/views/web/home.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php $this->load->view("_partials_web/head.php");?>  
+</head>
+
+<body>
+<!--Header-->
+<header>
+    <?php $this->load->view("_partials_web/header.php");?>
+</header>
+<!--Header-->
+<!-- banner-text -->
+        <div class="slider">
+            <div class="callbacks_container">
+                <ul class="rslides callbacks callbacks1" id="slider4">
+                    <li>
+                        <div class="banner-top">
+                        <div class="layer">
+                            <div class="container">
+                                <div class="banner-info_agile_w3ls">
+                                    <h2>DATA KTP</h2>
+                                    <p>-----------</p>
+                                    </div>
+                            </div>
+                        </div>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="banner-top1">
+                        <div class="layer">
+                            <div class="container">
+                                <div class="banner-info_agile_w3ls">
+                                    <h2>DATA KTP</h2>
+                                    <p>-----------</p>
+                                </div>
+                            </div>
+                        </div>
+                        </div>
+                    </li>
+                    
+                    
+                </ul>
+            </div>
+            <div class="clearfix"> </div>
+        </div>
+        <?php $this->load->view("_partials_web/footer.php");?>
+
+<script type="text/javascript" src="<?php echo base_url('js_web/jquery-2.2.3.min.js') ?>"></script>
+<script type="text/javascript" src="<?php echo base_url('js_web/bootstrap.js') ?>"></script>
+
+
+</body>
+</html>
